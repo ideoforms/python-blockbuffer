0cc9c8d0e107d37fdf52fda070f152020f233eeb
Daniel Jones <daniel.jones@sonos.com>
2020-05-14 20:51:42 +0100

    Initial commit

---
+++ blockbuffer/__init__.py
@@ -0,0 +1,22 @@
+"""
+
+BlockBuffer
+~~~~~~
+Buffer audio samples into fixed-sized blocks, with overlap.
+
+
+Usage
+~~~~~
+import blockbuffer
+bb = blockbuffer.BlockBuffer(block_size=1024,
+                             hop_size=128)
+bb.extend(array_of_samples)
+for block in bb.blocks:
+    assert len(block) == 1024
+"""
+
+from .blockbuffer import BlockBuffer
+from .exceptions import BlockBufferFullException, BlockBufferValueException
+
+__author__ = "Daniel Jones <http://www.erase.net/>"
+__all__ = [ "BlockBuffer" ]
