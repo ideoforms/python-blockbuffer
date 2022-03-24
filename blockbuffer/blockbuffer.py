843eb12272752a83b441f524f9d0008c8fd5362f
Daniel Jones <daniel@jones.org.uk>
2022-03-24 17:59:40 +0000

    Add support for 2D buffers

--- blockbuffer/blockbuffer.py
+++ blockbuffer/blockbuffer.py
@@ -1,20 +1,22 @@
 import numpy as np
 from .exceptions import BlockBufferValueException, BlockBufferFullException
 
-BLOCK_BUFFER_DEFAULT_CAPACITY_BLOCKS = 8
+BLOCK_BUFFER_DEFAULT_CAPACITY_BLOCKS = 64
 
 class BlockBuffer(object):
-    def __init__(self, block_size, hop_size=None, capacity=None, auto_resize=False):
+    def __init__(self, block_size, hop_size=None, num_channels=1, capacity=None, auto_resize=False):
         """
         Args:
             block_size: The number of samples to return per block.
             hop_size: The amount the read head should be moved forward per block.
+            num_channels: The number of channels to allocate.
             capacity: The total buffer capacity in samples. Defaults to block_size * 8.
             auto_resize: Automatically resize the buffer if it is extended beyond capacity.
                          Does memory allocation, so should not be used in real-time threads.
         """
         self.block_size = block_size
         self.hop_size = hop_size if hop_size is not None else block_size
+        self.num_channels = num_channels
         self.write_position = 0
         self.read_position = 0
         self.length = 0
@@ -34,8 +36,15 @@ class BlockBuffer(object):
         if self.capacity < self.block_size + self.hop_size:
             raise BlockBufferValueException("Capacity must be >= block_size + hop_size")
 
-        self.queue = np.zeros((self.capacity))
-        self.return_buffer = np.zeros((self.block_size))
+        #--------------------------------------------------------------------------------
+        # Ringbuffer to store the entire audio queue.
+        #--------------------------------------------------------------------------------
+        self.queue = np.zeros((self.capacity, self.num_channels))
+
+        #--------------------------------------------------------------------------------
+        # Used to store and return each buffer of audio.
+        #--------------------------------------------------------------------------------
+        self.return_buffer = np.zeros((self.block_size, self.num_channels))
 
     def __iter__(self):
         return self.blocks
@@ -46,29 +55,73 @@ class BlockBuffer(object):
         Safe for usage in real-time audio applications, as no memory allocation or system I/O is done
 
         Args:
-            frames: A 1D array of frames to process.
+            frames: An array of frames to process. Can be a 1D or 2D array, either Python
+                    native list or numpy.ndarray.
         """
-        if len(np.shape(frames)) > 1:
-            raise BlockBufferValueException("Block buffer currently only supports 1D data")
 
+        #--------------------------------------------------------------------------------
+        # Type checking and array validation.
+        #--------------------------------------------------------------------------------
+        if type(frames) == np.ndarray:
+            if frames.ndim > 2:
+                raise BlockBufferValueException("Invalid number of dimensions in frames")
+            elif (frames.ndim == 2 and frames.shape[1] != self.num_channels) or \
+                    (frames.ndim == 1 and self.num_channels > 1):
+                raise BlockBufferValueException("Invalid number of channels in frames (expected %d, got %d)" %
+                                 (self.num_channels, frames.shape[1]))
+        else:
+            if self.num_channels == 1:
+                if not isinstance(frames[0], (int, float)):
+                    raise BlockBufferValueException("Invalid number of dimensions in frames")
+            else:
+                if type(frames[0]) != list:
+                    raise BlockBufferValueException("Invalid number of dimensions in frames")
+                if len(frames[0]) != self.num_channels:
+                    raise BlockBufferValueException("Invalid number of channels in frames (expected %d, got %d)" %
+                                     (self.num_channels, len(frames[0])))
         num_frames = len(frames)
+
+        #--------------------------------------------------------------------------------
+        # Resize the buffer (if enabled)
+        #--------------------------------------------------------------------------------
         if self.length + num_frames > self.capacity:
             if self.auto_resize:
                 size_increase = self.length + num_frames - self.capacity
-                self.queue = np.pad(self.queue, (0, size_increase))
+                self.queue = np.pad(self.queue, ((0, size_increase), (0, 0)))
                 self.capacity += size_increase
             else:
                 raise BlockBufferFullException("Block buffer overflowed")
 
+        #--------------------------------------------------------------------------------
+        # Write the samples.
+        # Logic is complex due to having to jump through hoops to avoid memory
+        # allocations.
+        #--------------------------------------------------------------------------------
         self.write_position = self.write_position % self.capacity
 
         if self.write_position + num_frames <= self.capacity:
-            self.queue[self.write_position:self.write_position+num_frames] = frames
+            if self.num_channels == 1:
+                self.queue[self.write_position:self.write_position + num_frames, 0] = frames
+            elif type(frames) == np.ndarray:
+                self.queue[self.write_position:self.write_position + num_frames] = frames
+            else:
+                for index, row in enumerate(frames):
+                    self.queue[self.write_position + index] = row
         else:
             remaining_frames = self.capacity - self.write_position
-            self.queue[self.write_position:] = frames[:remaining_frames]
-            self.queue[:num_frames - remaining_frames] = frames[remaining_frames:]
+            if self.num_channels == 1:
+                self.queue[self.write_position:, 0] = frames[:remaining_frames]
+                self.queue[:num_frames - remaining_frames, 0] = frames[remaining_frames:]
+            elif type(frames) == np.ndarray:
+                self.queue[self.write_position:] = frames[:remaining_frames]
+                self.queue[:num_frames - remaining_frames] = frames[remaining_frames:]
+            else:
+                for index, row in enumerate(frames):
+                    self.queue[(self.write_position + index) % self.capacity] = row
 
+        #--------------------------------------------------------------------------------
+        # Update length and write position
+        #--------------------------------------------------------------------------------
         self.length += num_frames
         self.write_position = self.write_position + num_frames
 
@@ -90,7 +143,10 @@ class BlockBuffer(object):
                 rv = self.return_buffer
             self.length -= self.hop_size
             self.read_position = (self.read_position + self.hop_size) % self.capacity
-            return rv
+            if self.num_channels == 1:
+                return rv[:, 0]
+            else:
+                return rv
         return None
 
     @property
--- tests/test_blockbuffer.py
+++ tests/test_blockbuffer.py
@@ -6,61 +6,61 @@ from blockbuffer import BlockBufferFullException, BlockBufferValueException
 def test_blockbuffer_basic():
     bb = BlockBuffer(4)
     assert bb.get() is None
-    bb.extend([ 1, 2, 3, 4, 5, 6, 7, 8 ])
-    assert np.array_equal(bb.get(), [ 1, 2, 3, 4 ])
-    assert np.array_equal(bb.get(), [ 5, 6, 7, 8 ])
+    bb.extend([1, 2, 3, 4, 5, 6, 7, 8])
+    assert np.array_equal(bb.get(), [1, 2, 3, 4])
+    assert np.array_equal(bb.get(), [5, 6, 7, 8])
     assert bb.get() is None
 
 def test_blockbuffer_capacity():
     bb = BlockBuffer(4, capacity=8)
-    bb.extend([ 1, 2, 3, 4 ])
-    bb.extend([ 5, 6, 7, 8 ])
+    bb.extend([1, 2, 3, 4])
+    bb.extend([5, 6, 7, 8])
     with pytest.raises(BlockBufferFullException):
-        bb.extend([ 9 ])
+        bb.extend([9])
 
 def test_blockbuffer_auto_resize():
     bb = BlockBuffer(4, capacity=8, auto_resize=True)
-    bb.extend([ 1, 2, 3, 4 ])
+    bb.extend([1, 2, 3, 4])
     assert bb.capacity == 8
-    bb.extend([ 5, 6, 7, 8 ])
+    bb.extend([5, 6, 7, 8])
     assert bb.capacity == 8
-    bb.extend([ 9, 10, 11, 12 ])
+    bb.extend([9, 10, 11, 12])
     assert bb.capacity == 12
-    assert np.array_equal(bb.get(), [ 1, 2, 3, 4 ])
-    assert np.array_equal(bb.get(), [ 5, 6, 7, 8 ])
-    assert np.array_equal(bb.get(), [ 9, 10, 11, 12 ])
+    assert np.array_equal(bb.get(), [1, 2, 3, 4])
+    assert np.array_equal(bb.get(), [5, 6, 7, 8])
+    assert np.array_equal(bb.get(), [9, 10, 11, 12])
     assert bb.get() is None
 
 def test_blockbuffer_hop():
     bb = BlockBuffer(8, 2)
-    bb.extend([ 1, 2, 3, 4 ])
+    bb.extend([1, 2, 3, 4])
     assert bb.get() is None
-    bb.extend([ 5, 6, 7, 8 ])
-    assert np.array_equal(bb.get(), [ 1, 2, 3, 4, 5, 6, 7, 8 ])
-    bb.extend([ 9, 10, 11, 12 ])
-    assert np.array_equal(bb.get(), [ 3, 4, 5, 6, 7, 8, 9, 10 ])
-    assert np.array_equal(bb.get(), [ 5, 6, 7, 8, 9, 10, 11, 12 ])
+    bb.extend([5, 6, 7, 8])
+    assert np.array_equal(bb.get(), [1, 2, 3, 4, 5, 6, 7, 8])
+    bb.extend([9, 10, 11, 12])
+    assert np.array_equal(bb.get(), [3, 4, 5, 6, 7, 8, 9, 10])
+    assert np.array_equal(bb.get(), [5, 6, 7, 8, 9, 10, 11, 12])
     assert bb.get() is None
 
 def test_blockbuffer_hop_odd_capacity():
     bb = BlockBuffer(4, 2, capacity=6)
     assert bb.get() is None
-    bb.extend([ 1, 2, 3, 4 ])
-    assert np.array_equal(bb.get(), [ 1, 2, 3, 4 ])
+    bb.extend([1, 2, 3, 4])
+    assert np.array_equal(bb.get(), [1, 2, 3, 4])
     assert bb.get() is None
     bb.extend([5, 6, 7, 8])
-    assert np.array_equal(bb.get(), [ 3, 4, 5, 6 ])
-    assert np.array_equal(bb.get(), [ 5, 6, 7, 8 ])
+    assert np.array_equal(bb.get(), [3, 4, 5, 6])
+    assert np.array_equal(bb.get(), [5, 6, 7, 8])
     assert bb.get() is None
 
 def test_blockbuffer_iterator():
     bb = BlockBuffer(4, 2)
-    bb.extend([ 1, 2, 3, 4, 5, 6, 7, 8 ])
+    bb.extend([1, 2, 3, 4, 5, 6, 7, 8])
     blocks = list(bb)
     assert len(blocks) == 3
-    assert np.array_equal(blocks[0], [ 1, 2, 3, 4 ])
-    assert np.array_equal(blocks[1], [ 3, 4, 5, 6 ])
-    assert np.array_equal(blocks[2], [ 5, 6, 7, 8 ])
+    assert np.array_equal(blocks[0], [1, 2, 3, 4])
+    assert np.array_equal(blocks[1], [3, 4, 5, 6])
+    assert np.array_equal(blocks[2], [5, 6, 7, 8])
 
 def test_blockbuffer_bad_values():
     # invalid hop size
@@ -77,10 +77,34 @@ def test_blockbuffer_bad_values():
 
     # invalid capacity (must be >= block_size + hop_size)
     with pytest.raises(BlockBufferValueException):
-        bb = BlockBuffer(4, 4, 4)
+        bb = BlockBuffer(4, 4, capacity=4)
 
     # invalid data format (must be 1D)
     bb = BlockBuffer(4)
     with pytest.raises(BlockBufferValueException):
-        bb.extend(np.array([ [1, 2], [3, 4]]))
+        bb.extend(np.array([[1, 2], [3, 4]]))
 
+def test_blockbuffer_2D():
+    bb = BlockBuffer(2, num_channels=2)
+    bb.extend(np.array([[1, 2, 3, 4], [5, 6, 7, 8]]).T)
+    assert np.array_equal(bb.get(), np.array([[1, 5], [2, 6]]))
+    assert np.array_equal(bb.get(), np.array([[3, 7], [4, 8]]))
+    assert bb.get() is None
+
+    bb = BlockBuffer(2, num_channels=2)
+    bb.extend([[1, 5], [2, 6]])
+    assert np.array_equal(bb.get(), np.array([[1, 5], [2, 6]]))
+    assert bb.get() is None
+    bb.extend([[3, 7], [4, 8], [5, 9], [6, 10]])
+    assert np.array_equal(bb.get(), np.array([[3, 7], [4, 8]]))
+    assert np.array_equal(bb.get(), np.array([[5, 9], [6, 10]]))
+    assert bb.get() is None
+
+    bb = BlockBuffer(2, num_channels=2)
+    bb.extend(np.array([[1, 5], [2, 6]]))
+    assert np.array_equal(bb.get(), np.array([[1, 5], [2, 6]]))
+    assert bb.get() is None
+    bb.extend(np.array([[3, 7], [4, 8], [5, 9], [6, 10]]))
+    assert np.array_equal(bb.get(), np.array([[3, 7], [4, 8]]))
+    assert np.array_equal(bb.get(), np.array([[5, 9], [6, 10]]))
+    assert bb.get() is None
